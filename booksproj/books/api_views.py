24bc47cef64ec58603db31f298d4058b42e4ff7d
JacekKorta <jacek.korta@gmail.com>
2021-03-21 12:49:24 +0100

    Add sorting

--- booksproj/books/api_views.py
+++ booksproj/books/api_views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import FieldError
 from rest_framework.views import APIView, Response
 
 from .models import Author, Book
@@ -9,14 +10,23 @@ class BooksListView(APIView):
     def get_queryset(self):
         queryset = Book.objects.prefetch_related('authors').all()
         query_date = self.request.query_params.get('published_date', None)
-        query_authors = [author.replace("'", "").replace('"', '')
-                         for author in self.request.query_params.getlist('author', None)]
-
+        query_authors = [
+            author.replace("'", "").replace('"', '')
+            for author in self.request.query_params.getlist('author', None)
+        ]
+        sort = self.request.query_params.get('sort', None)
         if query_date is not None:
             queryset = queryset.filter(published_date=query_date)
-        elif query_authors is not None:
-            authors_ids = [author_id for author_id in [Author.objects.get(name=name).id for name in query_authors]]
+        if query_authors:
+            authors_ids = [
+                author.id for author in Author.objects.filter(name__in=query_authors)
+            ]
             queryset = queryset.filter(authors__in=authors_ids)
+        if sort is not None:
+            try:
+                queryset = queryset.order_by(sort)
+            except FieldError:
+                return queryset
         return queryset
 
     def get(self, request, format=None):
